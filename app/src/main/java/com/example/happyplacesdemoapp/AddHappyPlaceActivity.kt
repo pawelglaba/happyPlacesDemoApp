5817abc704ead87c84c9ce8d6cde29e6a5f984b2
pawelglaba <pawel.glaba@pwr.edu.pl>
2022-10-20 12:09:05 +0200

    HappyPlacesApp secondStage

---
+++ app/src/main/java/com/example/happyplacesdemoapp/AddHappyPlaceActivity.kt
@@ -0,0 +1,61 @@
+package com.example.happyplacesdemoapp
+
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_add_happy_place)
+
+        //click of it.)
+        // START
+        setSupportActionBar(toolbar_add_place) // Use the toolbar to set the action bar.
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // This is to use the home back button.
+        // Setting the click event to the back button
+        toolbar_add_place.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener {
+            view,year,month,dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            uptadeDateinView()
+
+        }
+        et_date.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id){
+            R.id.et_date -> {
+                DatePickerDialog(this@AddHappyPlaceActivity,dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH))
+                    .show()
+            }
+        }
+    }
+
+    private fun uptadeDateinView(){
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat,Locale.getDefault())
+        et_date.setText(sdf.format(cal.time).toString())
+    }
+}
--- app/src/main/java/com/example/happyplacesdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/happyplacesdemoapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.happyplacesdemoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       fabAddHappyPlace.setOnClickListener{
+           val intent = Intent(this,AddHappyPlaceActivity::class.java)
+          startActivity(intent)
+       }
     }
 }
